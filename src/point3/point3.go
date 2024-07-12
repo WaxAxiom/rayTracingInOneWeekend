1a3730ad30f15413a02545e8963715a63d06b085
Watson Carawon <111703833+WaxAxiom@users.noreply.github.com>
2024-07-12 20:11:22 +0000

    Refactor point3
    So far a point really just is a vector.

--- src/point3/point3.go
+++ src/point3/point3.go
@@ -1,22 +1,8 @@
 // Package point3 contains the Point3 type (an alias of Vec3) and supporting methods
 package point3
 
-type Point3 struct {
-	e [3]float64
-}
+import (
+	"main/src/vec3"
+)
 
-func (p Point3) X() float64 {
-	return p.e[0]
-}
-
-func (p Point3) Y() float64 {
-	return p.e[1]
-}
-
-func (p Point3) Z() float64 {
-	return p.e[2]
-}
-
-func New(e [3]float64) Point3 {
-	return Point3{e}
-}
+type Point3 = vec3.Vec3
