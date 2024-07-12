f30c88224f2ee3d3b40d9c98f7c62bec6dbcf3c1
Watson Carawon <111703833+WaxAxiom@users.noreply.github.com>
2024-07-12 20:11:32 +0000

    Add ray package

---
+++ src/ray/ray.go
@@ -0,0 +1,28 @@
+// Package ray contains the Ray type and supporting methods
+package ray
+
+import (
+	"main/src/point3"
+	"main/src/vec3"
+)
+
+type Ray struct {
+	origin    point3.Point3
+	direction vec3.Vec3
+}
+
+func (r Ray) Origin() point3.Point3 {
+	return r.origin
+}
+
+func (r Ray) Direction() vec3.Vec3 {
+	return r.direction
+}
+
+func (r Ray) At(t float64) point3.Point3 {
+	return vec3.Sum(r.origin, vec3.Scale(r.direction, t))
+}
+
+func New(p point3.Point3, v vec3.Vec3) *Ray {
+	return &Ray{p, v}
+}
